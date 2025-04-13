5f388da6040cd6515fe1c1ee147e887c5cbb5fc6
Gerard Pandji <gerarddhirren.pandji@my.jcu.edu.au>
2025-04-14 02:13:51 +0800

    Update HomeInventoryApp final with grid layout

---
+++ HomeInventoryApp/app/build/tmp/kapt3/stubs/debug/com/example/homeinventory/ui/screens/RoomLayoutScreenKt.java
@@ -0,0 +1,19 @@
+package com.example.homeinventory.ui.screens;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u00006\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0000\u001a\u0096\u0001\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00032\f\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u00062\b\u0010\b\u001a\u0004\u0018\u00010\u00032K\u0010\t\u001aG\u0012\u0013\u0012\u00110\u0003\u00a2\u0006\f\b\u000b\u0012\b\b\f\u0012\u0004\b\b(\r\u0012\u0013\u0012\u00110\u0003\u00a2\u0006\f\b\u000b\u0012\b\b\f\u0012\u0004\b\b(\u000e\u0012\u0013\u0012\u00110\u0003\u00a2\u0006\f\b\u000b\u0012\b\b\f\u0012\u0004\b\b(\u000f\u0012\u0004\u0012\u00020\u00010\n2\u0012\u0010\u0010\u001a\u000e\u0012\u0004\u0012\u00020\u0007\u0012\u0004\u0012\u00020\u00010\u0011H\u0007\u00a2\u0006\u0002\u0010\u0012\u001a\u0018\u0010\u0013\u001a\u00020\u00012\u0006\u0010\u0014\u001a\u00020\u00032\u0006\u0010\u0015\u001a\u00020\u0016H\u0007\u00a8\u0006\u0017"}, d2 = {"GridLayout", "", "rows", "", "cols", "placedItems", "", "Lcom/example/homeinventory/model/Item;", "selectedItemId", "onDrop", "Lkotlin/Function3;", "Lkotlin/ParameterName;", "name", "itemId", "x", "y", "onPlacedItemClick", "Lkotlin/Function1;", "(IILjava/util/List;Ljava/lang/Integer;Lkotlin/jvm/functions/Function3;Lkotlin/jvm/functions/Function1;)V", "RoomLayoutScreen", "roomId", "viewModel", "Lcom/example/homeinventory/viewmodel/SharedInventoryViewModel;", "app_debug"})
+@kotlin.OptIn(markerClass = {androidx.compose.material3.ExperimentalMaterial3Api.class})
+public final class RoomLayoutScreenKt {
+    
+    @androidx.compose.runtime.Composable
+    public static final void RoomLayoutScreen(int roomId, @org.jetbrains.annotations.NotNull
+    com.example.homeinventory.viewmodel.SharedInventoryViewModel viewModel) {
+    }
+    
+    @androidx.compose.runtime.Composable
+    public static final void GridLayout(int rows, int cols, @org.jetbrains.annotations.NotNull
+    java.util.List<com.example.homeinventory.model.Item> placedItems, @org.jetbrains.annotations.Nullable
+    java.lang.Integer selectedItemId, @org.jetbrains.annotations.NotNull
+    kotlin.jvm.functions.Function3<? super java.lang.Integer, ? super java.lang.Integer, ? super java.lang.Integer, kotlin.Unit> onDrop, @org.jetbrains.annotations.NotNull
+    kotlin.jvm.functions.Function1<? super com.example.homeinventory.model.Item, kotlin.Unit> onPlacedItemClick) {
+    }
+}
